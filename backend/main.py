d35171b95a3c4337e46197d6bbdfe914741129d3
MuhtasimApon <syedmuhtasimapon@gmail.com>
2026-06-13 21:08:49 +0600

    vercel kaam kore yaay

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://cse-2202-project.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )

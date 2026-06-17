e5fb8690f8728161e92b467bf13780cb92c337a1
MuhtasimApon <syedmuhtasimapon@gmail.com>
2026-06-17 07:52:29 +0600

    project page redirecting fix

--- frontend/app/dashboard/nav-items.ts
+++ frontend/app/dashboard/nav-items.ts
@@ -8,7 +8,7 @@ export const navItems: NavItem[] = [
   { id: 'dashboard', label: 'Dashboard', href: '/dashboard' },
   { id: 'notices', label: 'Notices', href: '/dashboard/notice-board' },
   { id: 'chatroom', label: 'Chatroom', href: '#' },
-  { id: 'projects', label: 'Projects', href: '#' },
+  { id: 'projects', label: 'Projects', href: '/dashboard/projects' },
   { id: 'classes', label: 'Classes', href: '#' },
   { id: 'achievements', label: 'Achievements', href: '#' },
   { id: 'profile', label: 'Profile', href: '/dashboard/profile' },
